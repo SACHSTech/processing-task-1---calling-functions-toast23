00a0bcd7a24a709abf3b2cf524fa193a50e19fdd
tyao2 <tony.yao23@ycdsbk12.ca>
2021-12-13 13:25:23 -0500

    assignment 1 but on vscode

--- Sketch.java
+++ Sketch.java
@@ -8,29 +8,40 @@ public class Sketch extends PApplet {
    */
   public void settings() {
 	// put your size call here
-    size(500, 500);
+    size(400, 400);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
-    background(210, 255, 173);
+    background(0, 255, 0);
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    // sky 
+    fill(0, 255, 255);
+    rect(0, 0, 400, 230);
+
+    // house 
+    fill(255, 205, 180);
+    rect(170, 220, 70, 45);
+
+    // roof
+    fill(165, 42, 42);
+    triangle(170, 220, 205, 195, 240, 220);
+
+    // window
+    fill(0, 0, 255);
+    rect(179, 230, 10, 12);
+    rect(220, 230, 10, 12);
+    
+    // door 
+    fill(165, 42, 42);
+    rect(200, 242, 10, 12);
+
+    // sun 
+    fill(255, 255, 0);
+    ellipse(0, 0, 150, 150);
+
   }
-  
-  // define other methods down here.
+
 }
